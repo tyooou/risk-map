41b7ddebea204b7e41c290fdee84550bfcd662c4
Tyler <young.h.tyler@gmail.com>
2024-05-18 17:33:12 +1200

    Added Path class to represent edge in graph.

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -1,3 +1,5 @@
+package nz.ac.auckland.se281;
+
 public class Country {
   private String name;
   private String continent;
---
+++ src/main/java/nz/ac/auckland/se281/Path.java
@@ -0,0 +1,7 @@
+package nz.ac.auckland.se281;
+
+public class Path {
+  private Country to;
+  private Country from;
+  private Integer tax;
+}
